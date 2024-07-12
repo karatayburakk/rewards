64f3350fa3552e622964fd700212aefac3921329
karatayburakk <54941630+karatayburakk@users.noreply.github.com>
2024-07-12 23:36:01 +0300

    signup and signin routes completed

---
+++ src/users/users.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
+import { UsersRepository } from './users.repository';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly usersRepository: UsersRepository) {}
+
+  getAllUsers(): Promise<User[]> {
+    return this.usersRepository.getAllUsers();
+  }
+}
